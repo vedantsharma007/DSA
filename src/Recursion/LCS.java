5b249f25a67131e28aa8a7686c5aeb3168db83bc
Vedant Sharma <sharmavedant644@gmail.com>
2026-01-29 20:36:22 +0530

    reducing space complexity in LCS

--- src/Recursion/LCS.java
+++ src/Recursion/LCS.java
@@ -4,6 +4,9 @@ package Recursion;
 // reduce time complexity of lcs by using memorization(dp)
 
 public class LCS {
+    // O(n) space complexity
+    //******************************************************************************************************************
+    /*
     public int longestCommonSubsequence(String text1, String text2) {
         int i=0,j=0;
         char[] X=text1.toCharArray();
@@ -16,7 +19,24 @@ public class LCS {
             return 1+lcs(X,i+1,Y,j+1);
         }
         return Math.max(lcs(X,i,Y,j+1),lcs(X,i+1,Y,j));
+    }*/
+    //******************************************************************************************************************
+
+    //second approach with O(1) space complexity
+    //******************************************************************************************************************
+    public int longestCommonSubsequence(String text1, String text2) {
+        int i=0,j=0;
+        return lcs(text1,i,text2,j);
     }
+    public int lcs(String text1,int i, String text2,int j){
+        if(i==text1.length() || j==text2.length()) return 0;
+        if(text1.charAt(i)==text2.charAt(j)){
+            return 1+lcs(text1,i+1,text2,j+1);
+        }
+        return Math.max(lcs(text1,i,text2,j+1),lcs(text1,i+1,text2,j));
+    }
+    //******************************************************************************************************************
+
     public static void main(String[] args){
         String text1="ABCDEFGHABIDH";
         String text2="ABJHVSHJVDHFH";
