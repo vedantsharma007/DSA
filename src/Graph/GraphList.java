e96f6c57a44988d8bb953803fe5b0ffbeb1dbf10
Vedant Sharma <sharmavedant644@gmail.com>
2026-02-02 19:00:20 +0530

    dfs in graph for only connected components

--- src/Graph/GraphList.java
+++ src/Graph/GraphList.java
@@ -17,21 +17,46 @@ public class GraphList {
         }
     }
 
+    //**************** add edges ***********************
+
+    public void addEdge(int src,int dest){
+        adj.get(src).add(dest);
+        adj.get(dest).add(src);
+    }
+
     //**************** dfs *******************
 
     public void dfs(int node){
         Stack<Integer> st=new Stack<>();
         boolean[] isVisited = new boolean[v];
         st.push(node);
-        isVisited[node]=true;
+
+        //for traversing disconnected components i need a loop from 0 to no.of nodes in which we call dfs for each node
+
         while(!st.isEmpty()){
-            int top=st.peek();
-            System.out.print(top+" ");
+            int top=st.pop();
+            if(isVisited[top]!=true){
+                System.out.print(top+" ");
+                isVisited[top]=true;
+            }
             for(Integer neighbour : adj.get(top)){
-                if()
+                if(!isVisited[neighbour]){
+                    st.push(neighbour);
+                }
             }
         }
     }
 
     //*************** main function *****************
+
+    public static void main(String[] args){
+        GraphList obj= new GraphList(6);
+        obj.addEdge(0,1);
+        obj.addEdge(1,2);
+        obj.addEdge(4,3);
+        obj.addEdge(1,3);
+        obj.addEdge(4,5);
+
+        obj.dfs(0);
+    }
 }
