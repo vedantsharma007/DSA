8ee936c65641076bde5044e9463947566505f7ea
Vedant Sharma <sharmavedant644@gmail.com>
2026-02-03 10:20:59 +0530

    dfs in weighted graph

--- src/Graph/GraphList.java
+++ src/Graph/GraphList.java
@@ -7,19 +7,41 @@ import java.util.Stack;
 public class GraphList {
     public int v;
     public List<List<Integer>> adj;  //can make linked list or arraylist
+    public List<List<Pair>> weightList;
 
+    public class Pair{
+        int node;
+        int weight;
+
+        //***** constructor ***********
+        Pair(int n,int w){
+            this.node=n;
+            this.weight=w;
+        }
+        @Override
+        public String toString(){
+            return "(" + node + "," + weight + ")" ;
+        }
+
+    }
     //******** constructor *********
     GraphList(int v){
         this.v=v;
         adj=new ArrayList<>();
+        weightList=new ArrayList<>();
+
         for(int i=0;i<v;i++){
             adj.add(new ArrayList<>());
+            weightList.add(new ArrayList<>());
         }
     }
 
     //**************** add edges ***********************
 
-    public void addEdge(int src,int dest){
+    public void addEdge(int src,int dest,int weight){
+
+        weightList.get(src).add(new Pair(dest,weight));
+        weightList.get(dest).add(new Pair(src,weight));
         adj.get(src).add(dest);
         adj.get(dest).add(src);
     }
@@ -51,12 +73,19 @@ public class GraphList {
 
     public static void main(String[] args){
         GraphList obj= new GraphList(6);
-        obj.addEdge(0,1);
-        obj.addEdge(1,2);
-        obj.addEdge(4,3);
-        obj.addEdge(1,3);
-        obj.addEdge(4,5);
+        obj.addEdge(0,1,10);
+        obj.addEdge(1,2,20);
+        obj.addEdge(4,3,40);
+        obj.addEdge(1,3,5);
+        obj.addEdge(4,5,50);
 
+        System.out.println("Weighted Adjacency List:");
+        for (int i = 0; i < obj.v; i++) {
+            System.out.println(i + " -> " + obj.weightList.get(i));
+        }
+
+        System.out.print("DFS order of given graph is: ");
         obj.dfs(0);
+
     }
 }
