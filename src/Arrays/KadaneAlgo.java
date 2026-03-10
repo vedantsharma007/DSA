4e778aa71f5928c187aea09e0762eba7281f6a85
Vedant Sharma <sharmavedant644@gmail.com>
2026-03-10 21:28:05 +0530

    kadane algo

---
+++ src/Arrays/KadaneAlgo.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class KadaneAlgo {
+    public int maxSubArray(int[] nums) {
+        int curr=nums[0];
+        int best=nums[0];
+        for(int i=1;i<nums.length;i++){
+            curr=Math.max(nums[i],nums[i]+curr);
+            best=Math.max(best,curr);
+        }
+        return best;
+    }
+
+    public static void main(String[] args) {
+        int[] nums={1,43,65,123,-231,45};
+        KadaneAlgo obj=new KadaneAlgo();
+        int ans= obj.maxSubArray(nums);
+        System.out.print("answer is: "+ans);
+    }
+}
